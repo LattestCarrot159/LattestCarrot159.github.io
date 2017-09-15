103daf451a924e70bd5a0b893b6b029dda81ccd1
LattestCarrot159 <lattestcarrot159@gmail.com>
2017-09-14 22:22:32 -0400

    Added every hero to the main page
    index.html is changed to allow main.js to populate it. Hours are now based on scrapped data from OWAPI and not hard coded (THANK GOD). resetheros.py is now put into one loop rather than two and mroe efficient.

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-$(function(){
-	$(".char").click(function(){
-		window.location.href = "heroPages/" + this.id + ".html";
-	});
+$(function (){
+	var charRequest = new XMLHttpRequest();
+	var $charWrapper = $('#charWrapper');
+	
+	charRequest.open('GET', 'https://owapi.net/api/v3/u/LatestCarrot-1839/heroes');	
+	charRequest.onload = function() {
+		var charData = JSON.parse(charRequest.responseText);
+		var character = charData.us.heroes.playtime.quickplay;
+		i = 0;
+		var times = Object.values(character)[i];
+		var timesFixed = times;
+		for (var key in character) {
+			times = Object.values(character)[i];
+			if (times < 1) {
+				if (times * 60 < 1){
+					timesFixed = times * 60 * 60 + ' Seconds';
+				}
+				else {
+					timesFixed = times * 60 + ' Minutes';
+				};
+			}
+			else {
+				timesFixed = times + ' Hours';
+			};
+			$charWrapper.append('<div class="char" id="' + key + '"><p class="charName">' + key + '<br>' + timesFixed + '</p></div><br>');
+			console.log(key + ', ' + timesFixed);
+			i++;
+		};
+	};
+	charRequest.send();
 });
--- resetheros.py
+++ resetheros.py
@@ -1,10 +1,14 @@
 import os
 for filename in os.listdir('resources/pics'):
-    f = open('main.css', 'a+')
-    f.write ('\n#' + os.path.splitext(filename)[0] + '{background-image:url("resources/pics/' + os.path.splitext(filename)[0] + '.png");}')
-    f.close()
-for filename in os.listdir('resources/pics'):
+    f = open('main.css')
+    if '#' + os.path.splitext(filename)[0] + '{background-image:url("resources/pics/' + os.path.splitext(filename)[0] + '.png");}' not in f.read():
+        f.close()
+        f = open('main.css', 'a')
+        f.write ('\n#' + os.path.splitext(filename)[0] + '{background-image:url("resources/pics/' + os.path.splitext(filename)[0] + '.png");}')
+        f.close()
+    else:
+        f.close()
     file = 'heroPages/' + os.path.splitext(filename)[0] + '.html'
-    f = open(file, 'w+')
-    f.write (open('resources/other/TEMPLATE.html').read().format(os.path.splitext(filename)[0]))
-    f.close()
+    fi = open(file, 'w+')
+    fi.write (open('resources/other/TEMPLATE.html').read().format(os.path.splitext(filename)[0]))
+    fi.close()
